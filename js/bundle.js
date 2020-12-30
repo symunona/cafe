31a65a5a0db9ff9598b033f13c51944cb77a5f65
Bendeguz <symunona@gmail.com>
2020-12-31 00:59:03 +0100

    All functional now. Added info page hash linkability legend universities & libraries

--- js/bundle.js
+++ js/bundle.js
@@ -26744,9 +26744,9 @@ return heatmapFactory;
     return HeatmapOverlay;
 });
 /* 
- * Leaflet Control Search v2.9.9 - 2020-12-01 
+ * Leaflet Control Search v2.9.7 - 2019-01-14 
  * 
- * Copyright 2020 Stefano Cudini 
+ * Copyright 2019 Stefano Cudini 
  * stefano.cudini@gmail.com 
  * http://labs.easyblog.it/ 
  * 
@@ -27319,7 +27319,7 @@ L.Control.Search = L.Control.extend({
       }
       else {
         //throw new Error("propertyName '"+propName+"' not found in marker"); 
-         
+        console.warn("propertyName '"+propName+"' not found in marker"); 
       }
     }
     else if(layer instanceof L.Path || layer instanceof L.Polyline || layer instanceof L.Polygon)
@@ -27338,7 +27338,7 @@ L.Control.Search = L.Control.extend({
       }
       else {
         //throw new Error("propertyName '"+propName+"' not found in shape"); 
-         
+        console.warn("propertyName '"+propName+"' not found in shape"); 
       }
     }
     else if(layer.hasOwnProperty('feature'))//GeoJSON
@@ -27354,12 +27354,12 @@ L.Control.Search = L.Control.extend({
           loc.layer = layer;			
           retRecords[ layer.feature.properties[propName] ] = loc;
         } else {
-          
+          console.warn("Unknown type of Layer");
         }
       }
       else {
         //throw new Error("propertyName '"+propName+"' not found in feature");
-         
+        console.warn("propertyName '"+propName+"' not found in feature"); 
       }
     }
     else if(layer instanceof L.LayerGroup)
@@ -27513,7 +27513,7 @@ L.Control.Search = L.Control.extend({
 
 		L.DomUtil.addClass(this._container, 'search-load');	
 
-		if(this._layer)
+		if(this.options.layer)
 		{
 			//TODO _recordsFromLayer must return array of objects, formatted from _formatData
 			this._recordsCache = this._recordsFromLayer();
@@ -27801,7 +27801,7 @@ requirejs([
   let osm = new L.TileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-      'opacity': 0.7,
+      'opacity': 1,
       'attribution': [attr_osm, attr_overpass].join(', ')
     }
   );
@@ -27810,15 +27810,40 @@ requirejs([
     .addLayer(osm)
     .setView(new L.LatLng(47.48942276367029, 19.05009221670925), 14);
 
+  map.on('moveend', (evt) => {
+    if (evt.target.getZoom() < 11) {
+      hideLayerData()
+    } else {
+      showData(cache)
+    }
+
+    var v = {
+      lat: map.getCenter().lat,
+      lng: map.getCenter().lng,
+      zoom: map.getZoom()
+    };
+    localStorage.setItem(POSITION_LS_KEY, JSON.stringify(v));
+
+    location.hash = `#${v.lat},${v.lng},${v.zoom}z`
+  })
+
+  function hideLayerData() {
+    Object.keys(interests).map((amenity) => {
+      layers[amenity].setData({ data: [], max: 2 })
+    })
+  }
+
+
+
   let cache = []
 
-  let cfg = {
+  let cfgGeneric = {
     // radius should be small ONLY if scaleRadius is true (or small radius is intended)
     // if scaleRadius is false it will be the constant radius used in pixels
-    "radius": RADIUS,
-    "maxOpacity": .5,
+    radius: RADIUS,
+    maxOpacity: .5,
     // scales the radius based on map zoom
-    "scaleRadius": true,
+    scaleRadius: true,
     // if set to false the heatmap uses the global maximum for colorization
     // if activated: uses the data maximum within the current map boundaries
     //   (there will always be a red spot with useLocalExtremas true)
@@ -27828,32 +27853,68 @@ requirejs([
     // which field name in your data represents the longitude - default "lng"
     lngField: 'lon',
     // which field name in your data represents the data value - default "value"
-    valueField: 'count'
-  };
+    valueField: 'count',
 
+    minOpacity: 0,
+    blur: .75
+  }
 
-  let heatmapLayer = new HeatmapOverlay(cfg);
+  const interests = {
+    cafe: {
+      radius: 0.0006,
+      maxOpacity: .8,
+      blur: 0.6,
+      gradient: {
+        '.3': '#a9973d',
+        '.5': 'brown',
+        '.95': 'yellow'
+      }
+    },
+    library: {
+      radius: 0.0008,
+      maxOpacity: .9,
+      blur: 0.5,
+      gradient: {
+        '.5': 'blue',
+        '.8': 'turquoise',
+        '.95': 'white'
+      }
+    },
+    university: {
+      maxOpacity: .7,
+      radius: 0.001,
+      gradient: {
+        '.5': 'red',
+        '.8': 'pink',
+        '.95': 'white'
+      }
+    },
+  }
 
-  // const types = [
-  //   {value: '[amenity=cafe]', text: 'Café' },
-  //   {value: '[amenity=library]', text: 'Library' },
-  //   {value: '[amenity=college]', text: 'College' }
-  // ]
+  let layers = {}
 
-  let searchFor = '[amenity=cafe]'
+  // Init heatmap layers.
+  Object.keys(interests).map((amenity) => {
+    layers[amenity] = new HeatmapOverlay(_.extend({}, cfgGeneric, interests[amenity]));
+    map.addLayer(layers[amenity])
+  })
 
   let cacheMap = {}
 
+  let queries = Object.keys(interests).map((amenity) => `node({{bbox}})[amenity=${amenity}];`).join('')
+
   let opl = new L.OverPassLayer({
-    // 'query': '(node({{bbox}})[amenity=cafe];node({{bbox}})[amenity=library];);out qt;',
-    query: `(node({{bbox}})${searchFor};);out qt;`,
-    // minZoomIndicatorEnabled: false,
+    query: `(${queries});out qt;`,
     minZoom: 13,
     minZoomIndicatorOptions: { minZoomMessage: 'Get closer to see the heatmap: CURRENTZOOM/MINZOOMLEVEL.' },
+
     onSuccess: function (data) {
       data.elements.map((e) => e.count = 1)
+
       // console.log('yeah', data.elements)
+
       document.getElementById('hit-count').textContent = `(${data.elements.length})`
+
       if (data.elements.length > 10000) {
         console.error('too much data...', data.elements.length)
         return;
@@ -27866,44 +27927,45 @@ requirejs([
           cache.push(obj)
         }
       })
-      heatmapLayer.setData({ data: cache, max: 2 });
+
+      showData(data.elements)
+
       return data
     },
   });
 
+  function showData(data) {
+    Object.keys(interests).map((amenity) => {
+      let amenityList = _.filter(data, (e) => e.tags.amenity === amenity)
+      // console.log('found', amenity, amenityList.length)
+      layers[amenity].setData({ data: amenityList, max: 2 })
+    })
+  }
+
   // var searchLayer = L.layerGroup().addTo(map);
   // map.addControl( new L.Control.Search({layer: searchLayer}) );
-	map.addControl( new L.Control.Search({
-		url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
-		jsonpParam: 'json_callback',
-		propertyName: 'display_name',
-		propertyLoc: ['lat','lon'],
-		marker: L.circleMarker([0,0],{radius:30}),
-		autoCollapse: true,
-		autoType: false,
-		minLength: 2
-	}) );
+  map.addControl(new LeafletSearch({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+    // jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat', 'lon'],
+    marker: L.circleMarker([0, 0], { radius: 30 }),
+    autoCollapse: true,
+    autoType: true,
+    minLength: 2
+  }));
 
   map.addLayer(opl)
 
-  map.addLayer(heatmapLayer)
+  if (location.hash && location.hash.split(',').length === 3){
+    let lat = parseFloat(location.hash.split(',')[0].substr(1))
+    let lng = parseFloat(location.hash.split(',')[1])
+    let zoom = parseInt(location.hash.split(',')[2])
+    map.setView(L.latLng(lat, lng), zoom, true);
+  }else{
+    restore(map)
+  }
 
-  restore(map)
-
-  map.on('moveend', (evt) => {
-    if (evt.target.getZoom() < 11) {
-      heatmapLayer.setData({ data: [], max: 2 });
-    } else {
-      heatmapLayer.setData({ data: cache, max: 2 });
-    }
-
-    var view = {
-      lat: map.getCenter().lat,
-      lng: map.getCenter().lng,
-      zoom: map.getZoom()
-    };
-    localStorage.setItem(POSITION_LS_KEY, JSON.stringify(view));
-  })
 
   function restore(map) {
     try {
@@ -27915,6 +27977,14 @@ requirejs([
     }
   }
 
+  window.toggleInfo = function () {
+    toggle(document.getElementById(status))
+  }
+
+  function toggle(elem){
+    elem.style.display = window.getComputedStyle(elem).display === 'block'?'none':'block'
+  }
+
   window.findMe = geoFindMe
 
   function geoFindMe() {
@@ -27922,23 +27992,19 @@ requirejs([
     function success(position) {
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
-
-      console.warn(position)
-
       map.setView(L.latLng(lat, lng), 13, true);
     }
 
     function error() {
-      status.textContent = 'Unable to retrieve your location';
+      console.error('Unable to retrieve your location');
     }
 
     if (!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
+      // status.textContent = 'Geolocation is not supported by your browser';
     } else {
-      status.textContent = 'Locating…';
+      // status.textContent = 'Locating…';
       navigator.geolocation.getCurrentPosition(success, error);
     }
-
   }
 
 
